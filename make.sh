3f74a52f82c98e6c04b33d83b0a487d0653c340e
Ben Palmer <Ben Palmer>
2015-09-29 16:14:52 +0100

    auto update

--- make.sh
+++ make.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd ${0%/*}
 workingDir=$(pwd)
-#src
-srcDir=$workingDir"/src"
 #Directories
+srcDir=$workingDir"/src"
 modDir=$workingDir"/mod"
 binDir=$workingDir"/bin"
 mkdir -p $modDir
@@ -18,7 +17,10 @@ libLink="-L"$libDirLib" -lBP"
 #-----------Library End--------
 fortLine="mpif90 -O3 -g -Wall -Wno-unused-function \
 -fbounds-check -fcheck=all -mtune=native "
-cd $srcDir
-commandLine=$fortLine" -J "$modDir"  -I"$modDirLib" testProg.f90 "$libLink" -o "$binDir"/test.x"
-eval $commandLine
+#cd $srcDir
+#commandLine=$fortLine" -J "$modDir"  -I"$modDirLib" "
+#commandLine=$commandLine" testProg.f90 "
+#commandLine=$commandLine" "$libLink" "
+#commandLine=$commandLine" -o "$binDir"/test.x"
+#eval $commandLine
 
